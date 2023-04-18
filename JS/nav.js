0b2e6915daceb84aaf45795e8b91d4fad6beebe3
VietAnh <vietanhnguyen0123456789@gmail.com>
2023-04-18 20:15:41 +0700

    da-fix-xong

--- JS/nav.js
+++ JS/nav.js
@@ -13,13 +13,13 @@ const createNav = () => {
                 <a href="#"><img src="IMG/cart.png" alt=""></a>
             </div>
         </div>
-        <ul class="links-container">
-            <li class="link-item"><a href="#" class="link">home</a></li>
-            <li class="link-item"><a href="#" class="link">women</a></li>
-            <li class="link-item"><a href="#" class="link">men</a></li>
-            <li class="link-item"><a href="#" class="link">kids</a></li>
-            <li class="link-item"><a href="#" class="link">accessories</a></li>
-        </ul>
+    <ul class="links-container">
+        <li class="link-item"><a href="#" class="link">home</a></li>
+        <li class="link-item"><a href="#" class="link">women</a></li>
+        <li class="link-item"><a href="#" class="link">men</a></li>
+        <li class="link-item"><a href="#" class="link">kids</a></li>
+        <li class="link-item"><a href="#" class="link">accessories</a></li>
+    </ul>
     `;
 }
 
